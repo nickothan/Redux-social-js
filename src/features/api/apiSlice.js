88a40d081f42aa932dd84463d413cc4c96e5b81c
Nickothan <s1nicolasrg@gmail.com>
2021-10-26 10:03:57 -0500

    Defining an API Slice

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+
+  baseQuery: fetchBaseQuery({ baseUrl: "/fakeApi" }),
+
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts"
+    })
+  })
+})
+
+export const { useGetPostsQuer } = apiSlice
